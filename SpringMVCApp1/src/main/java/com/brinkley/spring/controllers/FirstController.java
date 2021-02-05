3549d4080b8e7b6c77fad448b2ca7a8874cc476b
Brinckley <57827932+Brinckley@users.noreply.github.com>
2021-02-05 13:57:33 +0300

    model added

--- SpringMVCApp1/src/main/java/com/brinkley/spring/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/com/brinkley/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.brinkley.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,8 +12,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String HelloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message","Hello " + name + " " + surname);
+
         return "first/hello";
     }
 
