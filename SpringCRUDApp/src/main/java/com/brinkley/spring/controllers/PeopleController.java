ae8ad1165e695663d642d1956689b1c37c13735f
Brinckley <57827932+Brinckley@users.noreply.github.com>
2021-02-09 14:58:37 +0300

    adding people

--- SpringCRUDApp/src/main/java/com/brinkley/spring/controllers/PeopleController.java
+++ SpringCRUDApp/src/main/java/com/brinkley/spring/controllers/PeopleController.java
@@ -1,14 +1,17 @@
 package com.brinkley.spring.controllers;
 
 import com.brinkley.spring.dao.PersonDAO;
+import com.brinkley.spring.models.Person;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.jws.WebParam;
 
 @Controller
+@RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
@@ -24,7 +27,7 @@ public class PeopleController {
         return "check";
     }
 
-    @GetMapping("/people")
+    @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
@@ -35,5 +38,17 @@ public class PeopleController {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
 
 }
